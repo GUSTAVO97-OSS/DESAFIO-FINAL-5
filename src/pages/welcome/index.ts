f8aeab69254bcc1804cf01a3e5f992abe73ede47
Gustavo Nievas <gustavonievas246@gmail.com>
2022-05-29 19:06:57 -0300

    mas pruebas

--- src/pages/welcome/index.ts
+++ src/pages/welcome/index.ts
@@ -1,15 +1,22 @@
+import { goTo } from "../../router";
+import { state } from "../../state";
+
 export function initPageWelcome(params){
     const div = document.createElement("div")
     div.className = "welcome-page";
     div.innerHTML = `
     <custom-text variant="title">Piedra Papel ó Tijera</custom-text>
-    <custom-button class="elbutton">Empezar</custom-button>
+    <custom-button class="elbutton" path="/instructions">Empezar</custom-button>
     <custom-img>
     `
+    const root = document.querySelector(".root");
     const next = div.querySelector(".elbutton")
-    next.addEventListener("click", () => {
-        params.goTo("/instructions")
-    })
+    next.addEventListener("click", (e) => {
+		const target = e.target as any;
+		const route = target.getAttribute("path");
+		goTo(route, root);
+	});
+   
 
     return div;
 }
